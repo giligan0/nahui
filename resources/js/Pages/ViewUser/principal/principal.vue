f136d5e2a47913567752c1576f1df2c104f9c189
Fernando20V <jose.olivasvidea7@tecnacional.edu.ni>
2025-09-27 00:41:01 -0600

    fit: add component

---
+++ resources/js/Pages/ViewUser/principal/principal.vue
@@ -0,0 +1,13 @@
+<template>
+    <Dashboard />
+    <MainDash />
+
+</template>
+
+<script setup>
+import Dashboard from "../ViewUser/Dashboard.vue";
+import MainDash from "../ViewUser/MainDash.vue";
+
+    Dashboard
+    MainDash
+</script>
