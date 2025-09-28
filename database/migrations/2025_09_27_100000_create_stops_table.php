1cfc503edc34a02e2a67144fe6709ef4dad0d6c0
giligan0 <225043051+giligan0@users.noreply.github.com>
2025-09-27 23:20:51 -0600

    fix: changed stops coordinates datatype

--- database/migrations/2025_09_27_100000_create_stops_table.php
+++ database/migrations/2025_09_27_100000_create_stops_table.php
@@ -16,13 +16,17 @@ return new class extends Migration {
             $table->foreignId('municipality_id')->nullable()->constrained()->nullOnDelete();
             $table->foreignId('address_id')->nullable()->constrained()->nullOnDelete();
             $table->foreignId('place_id')->nullable()->constrained('places')->nullOnDelete();
-            $table->point('location')->nullable(); // TODO: enforce SRID 4326 in a later migration
+            // To do: grimzy/laravel-mysql-spatial which adds $table->point(). However, enforcing SRID 4326 requires raw SQL
+            // $table->point('location')->nullable(); // TODO: enforce SRID 4326 in a later migration
+            $table->decimal('lat', 10, 7);
+            $table->decimal('lng', 10, 7);
             $table->boolean('is_terminal')->default(false)->index();
             $table->boolean('is_active')->default(true); // covered by composite (municipality_id, is_active)
             $table->timestamps();
             $table->softDeletes();
             $table->index(['municipality_id', 'is_active']);
-            $table->spatialIndex('location');
+            $table->index(['lat', 'lng']);
+            // $table->spatialIndex('location');
         });
     }
 
