68d54faddd23fdfbdb7b58d4afecd4abb4443195
candida <candida.lanuza@tecnacional.edu.ni>
2025-09-29 09:30:15 -0600

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,25 @@ Route::get('/register', function () {
 //TODO PÁGINAS PRINCIPALES (autenticadas)
 Route::middleware('auth')->group(function () {
 
-    Route::resource('/accommodation_types',\App\Http\Controllers\Accommodation_typesController::class);
-    Route::resource('/accommodations',\App\Http\Controllers\AccommodationController::class);
-    Route::resource('/amenity_categories',\App\Http\Controllers\Amenity_categoriesController::class);
-    Route::resource('/amenities',\App\Http\Controllers\AmenitiesController::class);
-    Route::resource('/abailability_slots',\App\Http\Controllers\Abailability_slotsController::class);
-    Route::resource('/bookings',\App\Http\Controllers\BookingController::class);
-    Route::resource('/place_categories',\App\Http\Controllers\Place_categoryController::class);
-    Route::resource('/places',\App\Http\Controllers\PlaceController::class);
-    Route::resource('/restaurant_categories',\App\Http\Controllers\Restaurant_categoryController::class);
-    Route::resource('/dish_restaurants',\App\Http\Controllers\Dish_restaurantsController::class);
-    Route::resource('/accommodations',\App\Http\Controllers\AccommodationController::class);
 
+    Route::resource('/accommodation_types',\App\Http\Controllers\AccommodationTypesController::class);
+    Route::resource('/accommodations',\App\Http\Controllers\AccommodationController::class);
+    Route::resource('/amenity_categories',\App\Http\Controllers\AmenityCategoryController::class);
+    Route::resource('/amenities',\App\Http\Controllers\AmenityController::class);
+    Route::resource('/accommodation_amenities',\App\Http\Controllers\AccommodationAmenityController::class);
+    Route::resource('/abailability_slots',\App\Http\Controllers\AbailabilitySlotsController::class);
+    Route::resource('/bookings',\App\Http\Controllers\BookingController::class);
+    Route::resource('/place_categories',\App\Http\Controllers\PlaceCategoryController::class);
+    Route::resource('/places',\App\Http\Controllers\PlaceController::class);
+    Route::resource('/restaurant_categories',\App\Http\Controllers\RestaurantCategoryController::class);
+    Route::resource('/restaurants',\App\Http\Controllers\RestaurantController::class);
+    Route::resource('/dishes',\App\Http\Controllers\DishController::class);
+    Route::resource('/dish_restaurants',\App\Http\Controllers\DishRestaurantsController::class);
+    Route::resource('/event_categories',\App\Http\Controllers\EventCategoryController::class);
+    Route::resource('/events',\App\Http\Controllers\EventController::class);
     Route::resource('/reviews',\App\Http\Controllers\ReviewController::class);
+    Route::resource('/review_answers',\App\Http\Controllers\ReviewAnswerController::class);
+    Route::resource('/review_likes',\App\Http\Controllers\ReviewLikeController::class);
 
 
 
